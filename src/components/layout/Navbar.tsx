a079ce25adfcb49e6dfdba446fb5da5f5aff1cfc
James Llagas <jamesllagas82@gmail.com>
2026-06-11 08:42:52 +0800

    fix linking of my about section

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -60,7 +60,7 @@ export default function Navbar() {
         {/* DESKTOP LINKS */}
         <div className="hidden md:flex gap-8 font-medium">
           <a href="#home" className={linkClass("home")}>Home</a>
-          <a href="#about" className={linkClass("about")}>About</a>
+          <a href="#aboutme" className={linkClass("aboutme")}>About</a>
           <a href="#projects" className={linkClass("projects")}>Projects</a>
           <a href="#contact" className={linkClass("contact")}>Contact</a>
         </div>
